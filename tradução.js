e2351391a54a8405118b9acba4bebd247503d984
Matheusgit2 <matheusgluyz@gmail.com>
2025-06-05 19:19:13 -0300

    Update tradução.js

--- tradução.js
+++ tradução.js
@@ -8,7 +8,21 @@ async function changeLang(lang) {
       document.getElementById("title").textContent = data.title;
       document.getElementById("choose_period").textContent = data.choose_period;
       document.getElementById("greco_persian_wars").textContent = data.greco_persian_wars;
+      document.getElementById("battle_of_themorpilae").textContent = data.battle_of_themorpilae
       document.getElementById("marathon_battle").textContent = data.marathon_battle;
+       document.getElementById("ww1").textContent = data.ww1;
+       document.getElementById("Battle_of_Verdun").textContent = data.Battle_of_Verdun;
+      document.getElementById("Hundred_Days_Offensive").textContent = data.Hundred_Days_Offensive;
+      document.getElementById("ww2").textContent = data.ww2;
+      document.getElementById("Battle_of_Stalingrad").textContent = data.Battle_of_Stalingrad;
+      document.getElementById("Invasion_of_Normandy").textContent = data.Invasion_of_Normandy;
+      document.getElementById("site_description").textContent = data.site_description;
+      document.getElementById("future_updates").textContent = data.future_updates;
+      document.getElementById("image_caption").textContent = data.image_caption;
+      document.getElementById("legends").textContent = data.legends;
+
+
+
     } catch (err) {
       console.error("Erro ao traduzir:", err);
     }
