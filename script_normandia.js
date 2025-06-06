e9660b8bf1ab13e1b4f5cd4d22820ee59e03b568
Matheusgit2 <matheusgluyz@gmail.com>
2025-06-06 19:57:52 -0300

    Add files via upload

---
+++ script_normandia.js
@@ -0,0 +1,19 @@
+async function changeLang(lang) {
+try {
+      const res = await fetch(`${lang}.json`);
+      if (!res.ok) throw new Error("Erro ao carregar o JSON");
+      
+      const data = await res.json();
+  
+      document.getElementById("Invasion_of_Normandy").textContent = data.Invasion_of_Normandy;
+      document.getElementById("Normandy1").textContent = data.Normandy1;
+      document.getElementById("Normandy2").textContent = data.Normandy2;
+      document.getElementById("Normandy3").textContent = data.Normandy3;
+      document.getElementById("Normandy4").textContent = data.Normandy4;
+    } catch (err) {
+      console.error("Erro ao traduzir:", err);
+
+    }
+
+    
+}
