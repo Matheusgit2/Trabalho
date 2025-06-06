b36443bbc45a91d20efc5372e089128e82ebd682
Matheusgit2 <matheusgluyz@gmail.com>
2025-06-06 13:53:23 -0300

    Add files via upload

---
+++ Termófilas.js
@@ -0,0 +1,18 @@
+async function changeLang(lang) {
+    try {
+      const res = await fetch(`${lang}.json`);
+      if (!res.ok) throw new Error("Erro ao carregar o JSON");
+      
+      const data = await res.json();
+  
+      document.getElementById("sugestion").innerHTML = data.sugestion;
+      document.getElementById("title_thermo").textContent = data.title_thermo;
+       document.getElementById("main").textContent = data.main;
+      document.getElementById("description_thermo").textContent = data.description_thermo;
+      document.getElementById("myth_thermo").textContent = data.myth_thermo;
+       document.getElementById("myth_answer").textContent = data.myth_answer;
+    document.getElementById("Video").textContent = data.Video;
+    } catch (err) {
+      console.error("Erro ao traduzir:", err);
+    }
+}
