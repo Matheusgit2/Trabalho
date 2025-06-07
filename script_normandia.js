49a091cb83f106dde180e79c038b201aa54cdbbd
Matheusgit2 <matheusgluyz@gmail.com>
2025-06-06 22:48:12 -0300

    Update script_normandia.js

--- script_normandia.js
+++ script_normandia.js
@@ -1,4 +1,4 @@
-async function changeLang(lang) {
+window.changeLang=async function changeLang(lang) {
 try {
       const res = await fetch(`${lang}.json`);
       if (!res.ok) throw new Error("Erro ao carregar o JSON");
@@ -10,6 +10,7 @@ try {
       document.getElementById("Normandy2").textContent = data.Normandy2;
       document.getElementById("Normandy3").textContent = data.Normandy3;
       document.getElementById("Normandy4").textContent = data.Normandy4;
+      document.getElementById("Normandy5").textContent = data.Normandy5;
     } catch (err) {
       console.error("Erro ao traduzir:", err);
 
